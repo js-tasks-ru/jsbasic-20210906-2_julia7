df012bebba514f91cc261d50ca93a062f96d9282
julia <s98435@yandex.ru>
2021-08-30 15:00:27 +0300

    ++ 1-module 3-task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,11 @@
 function ucFirst(str) {
   // ваш код...
+  // if (str) {
+  //   str.toLowerCase()
+  //   return str[0].toUpperCase() + str.substring(1)
+  // } else {
+  //   return str
+  // }
+  str.toLowerCase()
+  return !!str ? str[0].toUpperCase() + str.substring(1) : str
 }
