8786dacf5b71a88643cb6b45a536737b50246467
julia <s98435@yandex.ru>
2021-08-30 12:42:20 +0300

    ++ 1-module 1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
   // ваш код...
+  if (n > 0) {
+    let fact = 1;
+    for(let i = n; i > 0; i--) {
+      fact *= i;
+    }
+    return fact
+  } else {
+    return n === 0 ? 1 : false
+  }
 }
