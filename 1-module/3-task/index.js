169e7f7010b5bb53ed67d320767bf83c0f69818a
julia <s98435@yandex.ru>
2021-08-30 15:28:04 +0300

    ++ 1-module 4-task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,11 +1,9 @@
 function ucFirst(str) {
   // ваш код...
   // if (str) {
-  //   str.toLowerCase()
   //   return str[0].toUpperCase() + str.substring(1)
   // } else {
   //   return str
   // }
-  str.toLowerCase()
   return !!str ? str[0].toUpperCase() + str.substring(1) : str
 }
