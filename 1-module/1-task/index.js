fd2655776a56bea004d05bdb0a8ef6418fe657b9
julia <s98435@yandex.ru>
2021-09-08 14:39:22 +0300

    ref: 1-module 1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,12 +1,11 @@
 function factorial(n) {
   // ваш код...
-  if (n > 0) {
+  if (n >= 0) {
     let fact = 1;
     for(let i = n; i > 0; i--) {
       fact *= i;
     }
     return fact
-  } else {
-    return n === 0 ? 1 : false
   }
+  return false
 }
