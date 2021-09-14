97327a5f56bd347431b83a9262af8bb84a3152e5
julia <s98435@yandex.ru>
2021-09-14 10:03:45 +0300

    change to isFinita() 2-module 1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,9 +1,8 @@
 function sumSalary(salaries) {
     let salary = 0;
     for(let item in salaries) {
-      if(salaries[item] > 0 && salaries[item] < Infinity && typeof salaries[item] == 'number') {
+      if (isFinite(salaries[item]) && typeof salaries[item] == 'number')
         salary += salaries[item]
       }
-    }
     return salary;
 }
