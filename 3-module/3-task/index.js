53ba4e4ae30b12a797a3ac567f5638673cdae87a
julia <s98435@yandex.ru>
2021-09-14 15:32:11 +0300

    +++ 3-module 5-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,5 +1,6 @@
 function camelize(str) {
 return str ? str.split('-')
+          .filter(item => item !== '-')
           .map(item => item[0].toUpperCase() + item.substr(1))
           .join('') : str
 }
