bad38bbddadcb2451dca7564f13255a726df34e2
julia <s98435@yandex.ru>
2021-09-15 12:58:39 +0300

    +++ 1-task and 4-task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,12 +1,3 @@
-// function showSalary(users, age) {
-//   let res = []
-//   users.filter(user => user.age <= age).forEach( el => {
-//     res.push(`${el.name}, ${el.balance}`)
-//   });
-//   return res.join('\n')
-// }
-
-
 function showSalary(users, age) {
   return users.filter(user => user.age <= age)
               .map( el => `${el.name}, ${el.balance}`)
