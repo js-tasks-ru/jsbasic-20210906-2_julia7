b9533a6ae91bd47b8e66da9c04e40be2de4c775e
julia <s98435@yandex.ru>
2021-10-05 15:25:31 +0300

    feat: 5-module

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
+const arrowRight = document.querySelector('.carousel__arrow_right');
+const arrowLeft = document.querySelector('.carousel__arrow_left');
+let maxlength = document.querySelectorAll('.carousel__slide').length;
+const carouselInner = document.querySelector('.carousel__inner');
+let widthCarousel = carouselInner.offsetWidth;
+let count = 0;
+let tranformLength = widthCarousel;
+arrowLeft.style.display = 'none';
+
 function initCarousel() {
-  // ваш код...
+  arrowRight.addEventListener('click', () => {
+    if (count == 0) {
+      if(tranformLength == 0) tranformLength = widthCarousel;
+      carouselInner.style.transform = `translateX(-${widthCarousel}px)`
+      count++
+      arrowLeft.style.display = '';
+    } else {
+      count++
+      tranformLength += widthCarousel;
+      carouselInner.style.transform = `translateX(-${tranformLength}px)`
+      if (count >= maxlength -1) {
+        arrowRight.style.display = 'none'
+      }
+    }
+  })
+
+  arrowLeft.addEventListener('click', () => {
+    count--
+    if(count < maxlength - 1) arrowRight.style.display = '';
+    if(count == 0) arrowLeft.style.display = 'none';
+    tranformLength -= widthCarousel;
+    carouselInner.style.transform = `translateX(-${tranformLength}px)`
+  })
 }
