ea9185c8e2cf5de5f5fc34c436aba71fa337b186
julia <s98435@yandex.ru>
2021-10-09 14:31:39 +0300

    feat: 6-module 2-task

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,5 +3,33 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.carouselInner = '';
+
+    slides.forEach(slide => {
+      this.carouselInner += `
+      <div class="carousel__slide" data-id="penang-shrimp">
+      <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
+      <div class="carousel__caption">
+        <span class="carousel__price">€${slide.price.toFixed(2)}</span>
+        <div class="carousel__title">${slide.name}</div>
+        <button type="button" class="carousel__button">
+          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+      </div>
+    </div>
+      `
+    this.carousel = `
+    <div class="carousel">
+      <div class="carousel__arrow carousel__arrow_right">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </div>
+      <div class="carousel__arrow carousel__arrow_left">
+        <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+      </div>
+      ${this.carouselInner}
+    </dis>
+    `
+    this.elem = createElement(this.carousel)
+    });
   }
 }
