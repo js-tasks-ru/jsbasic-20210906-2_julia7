04621ffbd6681113498351d34042373f61b0bc77
julia <s98435@yandex.ru>
2021-10-11 10:14:57 +0300

    6-module 3-task

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -4,10 +4,12 @@ export default class Carousel {
   constructor(slides) {
     this.slides = slides;
     this.carouselInner = '';
+    this.maxLength = slides.length;
+    this.count = 0;
 
     slides.forEach(slide => {
       this.carouselInner += `
-      <div class="carousel__slide" data-id="penang-shrimp">
+      <div class="carousel__slide" data-id="${slide.id}">
       <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
       <div class="carousel__caption">
         <span class="carousel__price">€${slide.price.toFixed(2)}</span>
@@ -18,6 +20,8 @@ export default class Carousel {
       </div>
     </div>
       `
+    });
+
     this.carousel = `
     <div class="carousel">
       <div class="carousel__arrow carousel__arrow_right">
@@ -26,10 +30,73 @@ export default class Carousel {
       <div class="carousel__arrow carousel__arrow_left">
         <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
       </div>
-      ${this.carouselInner}
+      <div class="carousel__inner">
+        ${this.carouselInner}
+      </div>
     </dis>
     `
     this.elem = createElement(this.carousel)
-    });
+    this.transformLength = this.widthCarousel
+
+    this.arrowRight.addEventListener('click', () => {
+      if (this.count == 0) {
+        if(this.transformLength == 0) this.transformLength = this.widthCarousel;
+        this.innerContainer.style.transform = `translateX(-${this.widthCarousel}px)`
+        this.count++
+        this.arrowLeft.style.display = '';
+      } else {
+        this.count++
+        this.transformLength += this.widthCarousel;
+        this.innerContainer.style.transform = `translateX(-${this.transformLength}px)`
+        if (this.count >= this.maxLength -1) {
+          this.arrowRight.style.display = 'none'
+        }
+      }
+    })
+
+    this.arrowLeft.addEventListener('click', () => {
+      this.count--
+      if(this.count < this.maxLength - 1) this.arrowRight.style.display = '';
+      if(this.count == 0) this.arrowLeft.style.display = 'none';
+      this.transformLength -= this.widthCarousel;
+      this.innerContainer.style.transform = `translateX(-${this.transformLength}px)`
+    })
+
+    this.btnPlus.forEach(slide => {
+      slide.addEventListener('click', this.btnClick)
+    })
   }
+
+  get arrowRight() {
+    return this.elem.querySelector('.carousel__arrow_right')
+  }
+  get arrowLeft() {
+    const left = this.elem.querySelector('.carousel__arrow_left')
+    left.style.display = 'none'
+    return left
+  }
+  get innerContainer() {
+    return this.elem.querySelector('.carousel__inner')
+  }
+
+  get widthCarousel() {
+    return this.innerContainer.offsetWidth
+  }
+
+  get btnPlus() {
+    return this.elem.querySelectorAll('.carousel__button')
+  }
+
+  btnClick = () => {
+    console.log(this)
+    const addProduct = new CustomEvent('product-add', {
+      detail: '',
+      bubbles: true
+    })
+
+    this.btnPlus.forEach(btn => {
+      btn.dispatchEvent(addProduct)
+    })
+  }
+
 }
