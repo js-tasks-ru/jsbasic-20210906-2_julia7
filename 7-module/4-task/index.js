64b900e31a1f6a48b5f249d66c8ad6b2bf3a1d21
julia <s98435@yandex.ru>
2021-10-15 20:53:24 +0300

    7-module 4-task

--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -1,4 +1,89 @@
+import createElement from '../../assets/lib/create-element.js';
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.segments = steps - 1
+    this.template = `
+    <div class="slider">
+      <div class="slider__thumb">
+        <span class="slider__value">${value}</span>
+      </div>
+      <div class="slider__progress"></div>
+      <div class="slider__steps">
+        <span class="slider__step-active"></span>
+      </div>
+    </div>
+    `
+    this.elem = createElement(this.template)
+    this.stepsSlider = this.elem.querySelector('.slider__steps')
+    this.sliderThumb = this.elem.querySelector('.slider__thumb')
+    this.sliderProgress = this.elem.querySelector('.slider__progress')
+
+    for(let i = 1; i < steps; i++) {
+      this.stepsSlider.innerHTML += '<span></span>'
+    }
+    this.stepSlider = this.stepsSlider.querySelectorAll('span')
+
+    this.elem.addEventListener('click', (event) => {
+      this.clickSlider(event)
+    })
+
+    this.sliderThumb.style.left = `${value / this.segments * 100}%`
+    this.sliderProgress.style.width = `${value / this.segments * 100}%`
+
+    this.sliderThumb.ondragstart = () => false;
+
+    this.sliderThumb.addEventListener('pointerdown', () => {
+      this.elem.classList.add('slider_dragging')
+
+      this.stepSlider.forEach(step => {
+        step.classList.remove('slider__step-active')
+      })
+
+      this.elem.addEventListener('pointermove', this.movePointer)
+
+      this.elem.addEventListener('pointerup', (event) => {
+        this.elem.removeEventListener('pointermove',this.movePointer)
+        this.stopPointer(event)
+      }, {once: true})
+    })
   }
+
+  movePointer = (event) => {
+    let moveLocation = ((event.clientX - this.elem.getBoundingClientRect().left) / this.elem.offsetWidth) * this.segments;
+    moveLocation = moveLocation > 4 ? 4 : moveLocation < 0 ? 0 : moveLocation
+    this.elem.querySelector('.slider__value').innerHTML = Math.round(moveLocation)
+    this.sliderThumb.style.left = `${moveLocation / this.segments * 100}%`
+    this.sliderProgress.style.width = `${moveLocation / this.segments * 100}%`
+  }
+
+  stopPointer = (event) => {
+    this.elem.classList.remove('slider_dragging')
+    let stopLocation = Math.round(((event.clientX - this.elem.getBoundingClientRect().left) / this.elem.offsetWidth) * this.segments);
+    this.elem.querySelector('.slider__value').innerHTML = Math.round(this.stopLocation)
+    this.sliderThumb.style.left = `${stopLocation / this.segments * 100}%`
+    this.sliderProgress.style.width = `${stopLocation / this.segments * 100}%`
+
+    this.elem.dispatchEvent(new CustomEvent('slider-change', {
+      detail: stopLocation,
+      bubbles: true
+    }))
+  }
+
+  clickSlider(event) {
+    this.stepSlider.forEach(step => {
+      step.classList.remove('slider__step-active')
+    })
+    let locationClick = Math.round(((event.clientX - this.elem.getBoundingClientRect().left) / this.elem.offsetWidth) * this.segments);
+    this.elem.querySelector('.slider__value').innerHTML = locationClick
+    this.stepSlider[locationClick].classList.add('slider__step-active')
+    this.sliderThumb.style.left = `${locationClick / this.segments * 100}%`
+    this.sliderProgress.style.width = `${locationClick / this.segments * 100}%`
+
+
+    this.elem.dispatchEvent(new CustomEvent('slider-change', {
+      detail: locationClick,
+      bubbles: true
+    }))
+  }
+
 }
